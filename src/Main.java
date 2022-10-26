0fc6bcf30e9f92d5ad00bebe151a415033e223a4
slicina7 <slicina1@etf.unsa.ba>
2022-10-26 23:29:42 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        Sat s = new Sat(20,59,59);
+        s.Ispisi();
+        s.Sljedeci();
+        s.Ispisi();
+        s.PomjeriZa(-61);
+        s.Ispisi();
+        s.Postavi(0,0,0);
+        s.Ispisi();
+
+    }
+}
